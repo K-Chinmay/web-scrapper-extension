0ed1b68478148fa601e7a87ca76a1d3055329c5a
K-Chinmay <chinmay99v@gmail.com>
2024-08-16 09:03:31 +0530

    resolving multiple downloads issue

--- popup.js
+++ popup.js
@@ -1,14 +1,5 @@
-document.getElementById("scrape-button")?.addEventListener("click", () => {
+document.getElementById("scrape-button").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tabs[0].id },
-        files: ["content.js"], // Injects the content script
-      },
-      () => {
-        // Send a message to the content script after it's injected
-        chrome.tabs.sendMessage(tabs[0].id, { action: "scrapeData" });
-      }
-    );
+    chrome.tabs.sendMessage(tabs[0].id, { action: "scrapeData" });
   });
 });
